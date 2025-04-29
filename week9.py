4feecc7eed33b51403c687333e5509b25ddd634f
Hugh Yoon <hw010603@gmail.com>
2025-04-29 16:49:22 +0900

    week9 update

---
+++ week9.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+
+x_data = [[1,2], [2,3], [3,1], [4,3], [5,3], [6,2]]
+y_data = [[0], [0], [0], [1], [1], [1]]
+
+x_train = torch.FloatTensor(x_data)
+y_train = torch.FloatTensor(y_data)
+
+model = nn.Sequential(
+    nn.Linear(2, 1),
+    nn.Sigmoid()
+)
+
+optimizer = optim.SGD(model.parameters(), lr=1)
+
+nb_epochs = 1000
+
+for epoch in range(nb_epochs + 1):
+    hypothesis = model(x_train)
+    cost = F.binary_cross_entropy(hypothesis, y_train)
+
+    optimizer.zero_grad()
+    cost.backward()
+    optimizer.step()
+    
+    if epoch % 100 == 0:
+        prediction = hypothesis >= torch.FloatTensor([0.5])
+        correct_prediction = prediction.float() == y_train
+        accuracy = correct_prediction.sum().item() / len(correct_prediction)
+        print(f"Epoch: {epoch} / {nb_epochs}, Cost: {cost.item()}, Accuracy: {accuracy}")
+
+
